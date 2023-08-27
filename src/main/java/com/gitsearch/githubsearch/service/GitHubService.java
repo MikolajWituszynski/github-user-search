b5f292e29659502493cae2388388fd188a1c7ddb
Mikolaj <mikolaj.wituszynski@gmail.com>
2023-08-27 09:58:17 +0200

    additon of commit sha and branch name correted

--- src/main/java/com/gitsearch/githubsearch/service/GitHubService.java
+++ src/main/java/com/gitsearch/githubsearch/service/GitHubService.java
@@ -10,6 +10,10 @@ import reactor.util.function.Tuple2;
 import reactor.util.function.Tuple3;
 import reactor.util.function.Tuples;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 @Service
 public class GitHubService {
 
@@ -19,25 +23,43 @@ public class GitHubService {
         this.webClient = webClientBuilder.baseUrl("https://api.github.com").build();
     }
 
-    public Flux<Tuple3<String, String,String>> getUsernameRepositoriesAndBranches(String username) {
+    public Flux<Tuple3<String, String, String>> getUsernameRepositoriesAndBranches(String username) {
+        String encodedUsername;
+        try {
+            encodedUsername = URLEncoder.encode(username, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            // Handle encoding exception
+            return Flux.error(e);
+        }
+
         return webClient.get()
-                .uri("/search/repositories?q=user:{username}+fork:false", username)
+                .uri("/search/repositories?q=user:"+encodedUsername+"+fork:false")
                 .retrieve()
                 .bodyToMono(GitHubSearchResponse.class)
                 .flatMapMany(response -> Flux.fromIterable(response.getItems()))
                 .flatMap(repository -> {
+                    // Repository name is not double-encoded here
+                    String encodedRepoName = repository.getRepoName();
+
                     return webClient.get()
-                            .uri("/repos/{username}/{repo}/branches", username, repository.getRepoName())
+                            .uri("/repos/"+ encodedUsername+"/"+ encodedRepoName+"/branches")
                             .retrieve()
                             .bodyToFlux(Branch.class)
                             .flatMap(branch -> {
-                                return webClient.get()
-                                        .uri("/repos/{username}/{repo}/branches", username, repository.getRepoName())
-                                        .retrieve()
-                                        .bodyToFlux(Branch.class)
-                                        .map(commit -> Tuples.of(repository.getRepoName(), branch.getName(), branch.getCommitSHA()));
-                            });
-                });
+                                return Flux.just(Tuples.of(repository.getRepoName(), branch.getName(), branch.getCommitSHA()));
+                            })
+                            .doOnError(error -> {
+                                System.err.println("Error fetching branches: " + error.getMessage());
+                            })
+                            .onErrorResume(throwable -> Flux.empty());
+                })
+                .doOnError(error -> {
+                    System.err.println("Error fetching repositories: " + error.getMessage());
+                })
+                .onErrorResume(throwable -> Flux.empty());
     }
 
+
 }
+
+
